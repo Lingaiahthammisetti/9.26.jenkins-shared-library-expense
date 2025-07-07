b0e08ac32f7f3146209a4e7aefbc368a379e22c2
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-07-07 18:54:39 +0530

    k8s jenkins shared library expense

--- vars/pipelineGlobals.groovy
+++ vars/pipelineGlobals.groovy
@@ -1,9 +1,9 @@
 static final String region() {
     return "us-east-1"
 }
-static final String nexusURL() {
-    return "nexus.lingaiah.online:8081"
-}
+// static final String nexusURL() {
+//     return "nexus.lingaiah.online:8081"
+// }
 static final String account_id() {
     return "484907532817"
 }
